39a83b888b32a233364b6b4ef8d81f791bf9eaa3
Asadulla Juraev <tezkormedia@gmail.com>
2021-11-28 10:06:53 +0500

    Initial Commit

---
+++ SwiftUI Post MVVM/SwiftUI_Post_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Post_MVVMApp.swift
+//  SwiftUI Post MVVM
+//
+//  Created by Asadulla Juraev on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Post_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
